830ab5b2aa5acbc60022b1b303eee9f989186320
Karl Dixon <karlandrewdixon@gmail.com>
2024-03-12 17:54:52 +0000

    ADD: Comment, Classes & Profile Models

--- forgefitnessweb/admin.py
+++ forgefitnessweb/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Classes, Profile, Comment
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Comment)
+admin.site.register(Classes)
--- forgefitnessweb/models.py
+++ forgefitnessweb/models.py
@@ -4,7 +4,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     #Name, Age, Gender, Phone, Email, Address, Current Plan 
+    user = models.OneToOneField(User, on_delete=models.CASCADE) #Delete profile when user is deleted
     name = models.CharField(max_length=100)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
     age = models.PositiveIntegerField()
     gender_choice = [
         ('M', 'Male'),
@@ -18,3 +20,28 @@ class Profile(models.Model):
     
     def __str__(self):
         return self.name
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+class Classes(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    location = models.CharField(max_length=200)
+    time = models.TimeField()
+    description = models.TextField(blank=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:  # Generate slug only if it's not already set
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.title} class at {self.location}, {self.time}"   
+    
