579a02f8600d0053f9981da5041f8160526085e2
Karlsberg62 <149387764+Karlsberg62@users.noreply.github.com>
2024-03-18 21:05:49 +0000

    Delete env.py

--- env.py
+++
@@ -1,7 +0,0 @@
-import os
-#Keep the DEBUG line to migrate to the local database. Comment the line out to migrate to the ElephantSQL Database.
-os.environ.setdefault("DEBUG","1")
-#Connection to ElephantSQL
-os.environ.setdefault("DB_URL",'postgres://ehziason:m51DJy5yioppEOqt8JxOTYlj-aDkLSLX@flora.db.elephantsql.com/ehziason')
-#Secret Key
-os.environ.setdefault("SK", '175986gjhit#!?34549')
