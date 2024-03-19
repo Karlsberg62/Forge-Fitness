aa41c8fc3b7730a04b00d065878458035228f670
Karl Dixon <karlandrewdixon@gmail.com>
2024-03-19 16:27:35 +0000

    Heroku deployment attempt & Profile Pic

--- forgefitness/settings.py
+++ forgefitness/settings.py
@@ -95,15 +95,16 @@ WSGI_APPLICATION = 'forgefitness.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if "DEBUG" in os.environ:
-    DATABASES = {
-            'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
+#if "DEBUG" in os.environ:
+#    DATABASES = {
+#            'default': {
+#            'ENGINE': 'django.db.backends.sqlite3',
+#            'NAME': BASE_DIR / 'db.sqlite3',
+#        }
+#   }
+#else:
+
+DATABASES = {
         'default': dj_database_url.parse(os.environ.get("DB_URL"))
     }
 
